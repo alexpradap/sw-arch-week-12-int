f32f8a756d5b42a6177e0171f4945566982ead73
Alexander Prada Perez <alexpradap@alexpradap-MacBook-Pro.local>
2023-10-29 01:13:46 -0500

    1234

---
+++ create_db_container.sh
@@ -0,0 +1,1 @@
+docker container create --name sw-arch-week-12-db --env MYSQL_ROOT_PASSWORD=mysql_root_Colombia1 -p 3306:3306 mysql:5.7
---
+++ create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `ventas` (
+  `id_cargue` int(11) NOT NULL AUTO_INCREMENT,
+  `empresa_local` varchar(20) NOT NULL,
+  `valor_venta` int(11) NOT NULL,
+  `fecha_venta` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_cargue`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
